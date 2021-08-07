2a4775b1f3e829277fb85f8d27bfa9d781d669bd
venkateshmaryala <venkateshmaryala@gmail.com>
2021-08-07 13:43:49 -0700

    added java files and modifed pom xml file with the relavent dependencies

---
+++ src/test/java/com/flipkart/CoreTest.java
@@ -0,0 +1,20 @@
+package com.flipkart;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class CoreTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
